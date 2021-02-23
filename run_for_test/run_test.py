23057671a02915a574b554f05e041733bb4f5961
walotta <walotta@126.com>
2021-02-23 13:44:38 +0800

    finish run_test.py

--- run_for_test/run_test.py
+++ run_for_test/run_test.py
@@ -7,24 +7,40 @@ for item in DataName:
         continue
     os.system('cp ../storage/' + item + '/code.cpp run_cpp.cpp')
     os.system('cp ../storage/' + item + '/answer.txt answer.txt')
-    print('\n\n======begin building binary file to run======\n\n')
-    os.system('cmake CMakeLists.txt')
-    os.system('make')
-    print('\n\n======begin running======')
+    print('======begin buildingProgram======')
+    os.system('cmake CMakeLists.txt > log.txt')
+    os.system('make > log.txt')
+    print('======begin running======\n')
     os.system('./test > run_ans.txt')
     print('======begin diff======')
     os.system('diff run_ans.txt answer.txt')
-    print('======diff end======\n\n')
+    print('======diff end======\n')
 
-memcheckFlag = input('Do you want to do memcheck [y/n] : ')
+memcheckFlag = input('Do you want to do memcheck [y/n/q] : ')
 if memcheckFlag == 'y':
     for item in DataName:
         check = input('this is ' + item + ' memcheck test, enter y/n to run : ')
         if check != 'y':
             continue
-        print('\n\n======begin building binary file to run======\n\n')
-        os.system('cmake CMakeLists.txt')
-        os.system('make')
-        print('\n\n======begin running valgrind======\n\n')
+        os.system('cp ../storage/' + item + '/code.cpp run_cpp.cpp')
+        os.system('cp ../storage/' + item + '/answer.txt answer.txt')
+        print('======begin buildingProgram======')
+        os.system('cmake CMakeLists.txt > log.txt')
+        os.system('make > log.txt')
+        print('======begin running valgrind======')
         os.system('valgrind --tool=memcheck --leak-check=full ./test')
-        print('======valgrind end======\n\n')
+        print('======valgrind end======\n')
+if memcheckFlag == 'q':
+    print('run memcheck quiet now')
+    for item in DataName:
+        check = input('this is ' + item + ' memcheck test, enter y/n to run : ')
+        if check != 'y':
+            continue
+        os.system('cp ../storage/' + item + '/code.cpp run_cpp.cpp')
+        os.system('cp ../storage/' + item + '/answer.txt answer.txt')
+        print('======begin buildingProgram======')
+        os.system('cmake CMakeLists.txt > log.txt')
+        os.system('make > log.txt')
+        print('======begin running valgrind======')
+        os.system('valgrind --tool=memcheck --leak-check=full -q ./test ')
+        print('======valgrind end======\n')
